f5f1fa797739dbe463319c4767b8eb691122ee24
owifasawaveg23-code <owifasawaveg23@gmail.com>
2026-03-25 12:59:59 +0600

    Update groupConnected.js

--- Nayan/lib/groupConnected.js
+++ Nayan/lib/groupConnected.js
@@ -3,24 +3,7 @@ module.exports = function () {
 
 Assalamualaykum☘️
 <------------------------------>
-BOT CONNECTED SUCCESSFUL !!!
-
-APPROVAL ALLOW IN THIS GROUP!!!
+BOT CONNECTED SUCCESSFUL !!
 <------------------------------>
-
-TO VIEW ANY COMMAND Use👉 ${global.config.PREFIX}help
-See command👉 ${global.config.PREFIX}menu
-
-EXAMPLE:👇
-${global.config.PREFIX}admin (information)
-${global.config.PREFIX}info (admin info)
-${global.config.PREFIX}alldown (link)
-
-<------------------------------>
-
-AND FOR ANY COMPLAINTS OR CONTACT BOT OPERATOR 
-
-DEVELOPER: ${global.config.botOwner}
-
-🔵WHATSAPP NUMBER: wa.me/+${global.config.admin[0] || "8801615298449"}`;
+🔵WHATSAPP NUMBER: wa.me/+${global.config.admin[0] || "8801838569277"}`;
 };
